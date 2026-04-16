a4a705f4900b8b64d0d2ff6d71a37f24a73c4fba
Sathya In <sathyasudar1705@gmail.com>
2026-04-16 14:41:02 +0530

    new

--- app.js
+++ app.js
@@ -16,7 +16,13 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+  
+  // Handle pre-flight OPTIONS request
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  
   next();
 });
 
